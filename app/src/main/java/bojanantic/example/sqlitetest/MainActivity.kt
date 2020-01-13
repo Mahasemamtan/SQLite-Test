977563374b65444fc18cdc5ebe36d6ebe5fe7b44
Bojan Antic <maatori90@gmail.com>
2020-01-13 22:50:21 +0100

    -- created simple table with two entries

--- app/src/main/java/bojanantic/example/sqlitetest/MainActivity.kt
+++ app/src/main/java/bojanantic/example/sqlitetest/MainActivity.kt
@@ -1,12 +1,16 @@
 package bojanantic.example.sqlitetest
 
+import android.content.ContentValues
+import android.content.Context
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +18,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        val database =
+            baseContext.openOrCreateDatabase("sqlite-test-1.db", Context.MODE_PRIVATE, null)
+        var sql = "CREATE TABLE contacts(_id INTEGER PRIMARY KEY NOT NULL, name TEXT, phone INTEGER, email TEXT)"
+        Log.d(TAG, ".onCreate: sql is $sql")
+        database.execSQL(sql)
+
+        sql = "INSERT INTO contacts(name, phone, email) VALUES('bojan', 1234123, 'bojan@email.com')"
+        Log.d(TAG, ".onCreate: sql is $sql")
+        database.execSQL(sql)
+
+        val values = ContentValues().apply {
+            put("name", "fred")
+            put("phone", 123123)
+            put("email", "fred@emial.com")
+        }
+
+        /** This code is equivalent to the code on lines {32 - 36} **/
+//        val value = ContentValues()
+//        value.put("name", "fred")
+//        value.put("phone", 123123)
+//        value.put("email", "fred@emial.com")
+
+        val generatedId = database.insert("contacts", null, values)
+        Log.d(TAG, ".onCreate record added with id:  $generatedId")
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
